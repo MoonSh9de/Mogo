d90e06060fe4a512f7a9d4007893bed129eae43f
MoonSh9de <krivoshein.w@gmail.com>
2023-08-05 22:40:43 +0300

    Added smooth scrolling to blocks. Fixed display of elements in the 'Wedo' block in the adaptive version.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+//* Header vars
 const header = $('#header');
 const headerLogo = $('#header-logo');
-//data-scroll
+
+//* Data-scroll vars
 const intro = $('#intro');
 const about = $('#about');
 const services = $('#services');
@@ -9,25 +11,23 @@ const blog = $('#blog');
 const contact = $('#contact');
 const blocksArray = [intro, services, about, work, blog, contact];
 
+//* Links var
 const navLinks = $('.nav__link')
 
+// *Menu nav toggle
+const nav = $("#nav");
+const nav__toggle = $("#nav-toggle");
+const burger = $("#burger");
+const x_mark = $("#x-mark");
+
+//* Scroll var
 let scrollOffset = $(window).scrollTop();
 
-window.addEventListener('scroll', function  (){
-    scrollOffset = $(this).scrollTop()
-    if(scrollOffset > intro.innerHeight()) {
-        header.addClass("fixed");
-         addActiveToBlocks();
-    }
-    else {
-        header.removeClass('fixed');
-    }
-});
-
-$("[data-scroll]").on('click', function () {
-    navLinks.removeClass('active');
-    $(this).addClass('active');
-});
+// *Functions
+const addHiddenClass = function() {
+    burger.toggleClass('hidden');
+    x_mark.toggleClass('hidden');
+}
 
 const addActiveToBlocks = function() {
     for (let i = 0; i < blocksArray.length; i++) {
@@ -43,92 +43,90 @@ const addActiveToBlocks = function() {
     }
 }
 
-  /*slider*/
-   $("[data-slider]").slick({
+function eventScroll() {
+
+    scrollOffset = $(this).scrollTop()
+    if(scrollOffset > intro.innerHeight()) {
+        header.addClass("fixed");
+         addActiveToBlocks();
+    }
+    else {
+        header.removeClass('fixed');
+    }
+}
+
+//* Hovers
+headerLogo.hover(function() {
+    headerLogo.toggleClass('fa-beat-fade');
+});
+
+burger.hover(function() {
+    burger.toggleClass('fa-beat-fade');
+});
+
+x_mark.hover(function() {
+    x_mark.toggleClass('fa-spin');
+});
+
+nav__toggle.on('click', function() {
+    addHiddenClass();
+});
+
+//* Events
+
+const eventForNav = new Event('checkNavActive')
+//* Scroll
+window.addEventListener('scroll', eventScroll);
+
+$("[data-scroll]").on('click', function () {
+    navLinks.removeClass('active');
+    $(this).addClass('active');
+});
+
+//* Smooth scroll
+$("[data-scroll]").on("click",function(event)
+{
+    event.preventDefault();
+    const blockID = $(this).data('scroll');
+    const element = document.getElementById(blockID);
+    const blockOffSet = element.offsetTop;
+    navLinks.removeClass("active")
+    if (document.documentElement.clientWidth <= 990) {
+        nav.toggleClass('active');
+        addHiddenClass();
+    }
+    //* Animate for scroll
+    if(blockID !== 'intro') {
+        $("html,body").animate({
+            scrollTop:blockOffSet + 40},700);
+    }
+    else  $("html,body").animate({
+        scrollTop:blockOffSet},700);
+
+});
+
+//* Slider
+$("[data-slider]").slick({
     infinite:true,
     fade:false,
     slidesToShow:1,
     slidesToScroll:1,
-   });
+});
 
-   /*Accordion block:*/
-   $("[data-collapse]").on("click",function(event){
+//* Accordion block
+$("[data-collapse]").on("click",function(event){
     event.preventDefault();
     let $this=$(this);
     let blockID = $(this).data("collapse");
     $(blockID).slideToggle();
     $(this).toggleClass("active")
-   });
+});
 
-// $(function()
-// {
-//     var header=$("#header"); //в переменную header сохраняем селектор header
-//     var introH=$("#intro").innerHeight(); //в переменную introH сохраняем высоту блока intro
-//     var scrollOffset=$(window).scrollTop(); //переменная,котороя хранит текущий скролл страницы в пикселях
-//     checkScroll(scrollOffset); //вызов функции checkScroll
-//     $(window).on("scroll load resize" , function()//при скроле работает функция
-//     {
-//         scrollOffset=$(this).scrollTop();
-//         checkScroll(scrollOffset);
-//     });
-//     /*Fixed header*/
-//     function checkScroll(scrollOffset)
-//     {
-//         if(scrollOffset>=introH )
-//         {
-//             header.addClass("fixed"); //присваивает селктору header значение fixed из css
-//         }
-//         else
-//         {
-//             header.removeClass("fixed"); //возвращает значение от fixef к header
-//         }
-//     }
-
-   //? Smooth scroll
-//    $("[data-scroll]").on("click",function(event)
-//    {
-//     event.preventDefault();
-//     let $this=$(this);
-//     let blockID = $(this).data('scroll');
-//     let blockOffSet=$(blockID).offset().top;
-
-// *    const nav__link = $('.nav__link');
-//     nav__link.removeClass("active")
-//     nav__toggle.addClass("active");
-//     $("html,body").animate({
-//         scrollTop:blockOffSet - 20},500);
-//    });
-
-   /*Menu nav toggle*/
-    const  nav = $("#nav");
-    const nav__toggle = $("#nav-toggle");
-    const burger = $("#burger");
-    const x_mark = $("#x-mark");
-
-    nav__toggle.on("click", function(event){
-        event.preventDefault();
-        console.log(nav__toggle.text());
-        $(this).toggleClass("active");
-        nav.toggleClass("active")
-   });
-
-    //Hovers
-    headerLogo.hover(function() {
-        headerLogo.toggleClass('fa-beat-fade');
-    });
-
-    burger.hover(function() {
-        burger.toggleClass('fa-beat-fade');
-    });
-
-    x_mark.hover(function() {
-        x_mark.toggleClass('fa-spin');
-    });
-
-    nav__toggle.on('click', function() {
-        burger.toggleClass('hidden');
-        x_mark.toggleClass('hidden');
-    });
-
+//* Nav__toggle
+nav__toggle.on("click", function(event){
+    event.preventDefault();
+    $(this).toggleClass("active");
+    nav.toggleClass("active")
+});
 
 
